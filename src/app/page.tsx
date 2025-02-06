4847c285c0390355ed835214664996400f38c0df
nachochiappero <ignaciogabrielchiappero@gmail.com>
2025-02-06 13:44:47 -0300

    add 3d pages transition scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
+import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
-import { clients } from "@/lib/constant";
+import { clients, products } from "@/lib/constant";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <Navbar/>
+      <Navbar />
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
 
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
-
-        <ContainerScroll
+          <ContainerScroll
             titleComponent={
               <div className="flex items-center flex-col">
                 <Button
-                  size={'lg'}
+                  size={"lg"}
                   className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
                 >
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
@@ -32,9 +32,6 @@ export default function Home() {
             }
           />
         </div>
-
-
-
       </section>
 
       <InfiniteMovingCards
@@ -44,8 +41,9 @@ export default function Home() {
         speed="slow"
       />
 
-
-
+      <section>
+      <HeroParallax products={products} />
+      </section>
     </main>
   );
 }
